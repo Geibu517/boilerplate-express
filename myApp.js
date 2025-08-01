683db0dee26241e8eb99d9b1652d71b1963cf456
Tienzo, Gabriel <gabe17dragneel@gmail.com>
2025-08-01 14:11:38 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,16 +1,21 @@
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables
+
 let express = require('express');
 let app = express();
 
+// Serve static files
 app.use("/public", express.static(__dirname + "/public"));
 
+// Serve index.html on root
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+// JSON route using environment variable
 app.get("/json", function(req, res) {
   let message = "Hello json";
 
+  // Check MESSAGE_STYLE and transform if needed
   if (process.env.MESSAGE_STYLE === "uppercase") {
     message = message.toUpperCase();
   }
