ed68b875c68376fd6381ac0d87461e4f5303d881
Tienzo, Gabriel <gabe17dragneel@gmail.com>
2025-08-01 13:58:11 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let app = express();
 
+// Serve static assets from the "public" directory
+app.use("/public", express.static(__dirname + "/public"));
+
 // Serve index.html for GET requests to "/"
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
